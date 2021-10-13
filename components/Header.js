ce1c089fc769cd02254a34010b86562fa8c4b3c8
areddin409 <areddin409@gmail.com>
2021-10-13 13:43:54 -0500

    Added feed component, started working on the stories component pulling fake user data with FakerJs

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ import { HomeIcon } from '@heroicons/react/solid';
 
 function Header() {
   return (
-    <div>
-      <div className='flex justify-between max-w-6xl '>
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
+      <div className='flex justify-between max-w-6xl mx-5 xl:mx-auto'>
         {/* left */}
         <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
           <Image
@@ -44,7 +44,25 @@ function Header() {
         </div>
 
         {/* right */}
-        {/* <HomeIcon /> */}
+        <div className='flex items-center justify-end space-x-4'>
+          <HomeIcon className='navBtn' />
+          <MenuIcon className='h-6 md:hidden cursor-pointer' />
+          <div className='relative navBtn'>
+            <PaperAirplaneIcon className='navBtn rotate-45' />
+            <div className='absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className='navBtn' />
+          <UserGroupIcon className='navBtn' />
+          <HeartIcon className='navBtn' />
+
+          <img
+            src='https://links.papareact.com/3ke'
+            alt='profile pic'
+            className='h-10 rounded-full cursor-pointer'
+          />
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
+import Feed from '../components/Feed';
 import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
       <Head>
         <title>Instagram 2.0</title>
         <link rel='icon' href='/instagram2.png' />
@@ -11,7 +12,7 @@ export default function Home() {
 
       <Header />
 
-      {/* Feed */}
+      <Feed />
 
       {/* Model */}
     </div>
