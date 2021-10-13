4ad5a8eefaeac476242c672898f60d88c567bc04
Andrew Reddin <areddin409@gmail.com>
2021-10-13 15:25:36 -0500

    created feed component with stories, posts,mini profile, and suggestions

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,7 @@
+import MiniProfile from './MiniProfile';
+import Posts from './Posts';
 import Stories from './Stories';
+import Suggestions from './Suggestions';
 
 function Feed() {
   return (
@@ -7,11 +10,16 @@ function Feed() {
         {/* Stories */}
         <Stories />
         {/* Posts */}
+        <Posts />
       </section>
 
-      <section>
-        {/* Mini Profile */}
-        {/* Suggestions */}
+      <section className='hidden xl:inline-grid md:col-span-1'>
+        <div className='fixed top-20'>
+          {/* Mini Profile */}
+          <MiniProfile />
+          {/* Suggestions */}
+          <Suggestions />
+        </div>
       </section>
     </main>
   );
