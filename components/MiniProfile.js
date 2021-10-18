0d6d023bca8895b6a584b60b1466ee6225777fc1
Andrew Reddin <areddin409@gmail.com>
2021-10-18 10:48:47 -0500

    add model to upload image to post and stored in firestore on the backend, added like/comment functionality

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -3,8 +3,6 @@ import { useSession, signOut } from 'next-auth/react';
 function MiniProfile() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return (
     <div className='flex items-center justify-between mt-14 ml-10'>
       <img
--- components/Posts.js
+++ components/Posts.js
@@ -1,40 +1,32 @@
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import Post from './Post';
 
-const posts = [
-  {
-    id: '123',
-    username: 'areddin409',
-    userImg: 'http://links.papareact.com/3ke',
-    img: 'https://images.unsplash.com/photo-1493612276216-ee3925520721?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80',
-    caption: 'This is DOPE!'
-  },
-  {
-    id: '1234',
-    username: 'areddin409',
-    userImg: 'http://links.papareact.com/3ke',
-    img: 'https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    caption: 'This is DOPE!'
-  },
-  {
-    id: '12345',
-    username: 'areddin409',
-    userImg: 'http://links.papareact.com/3ke',
-    img: 'https://images.unsplash.com/photo-1603290989063-b255b11b2525?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    caption: 'This is DOPE!'
-  }
-];
-
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
